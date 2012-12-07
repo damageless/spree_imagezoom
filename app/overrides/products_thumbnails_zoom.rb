489889e451ecf17b6bbf2012859e95b9068756d9
damageless <damageless@gmail.com>
2012-12-07 10:06:54 -0700

    Updated div class for thumbnails... again

--- app/overrides/products_thumbnails_zoom.rb
+++ app/overrides/products_thumbnails_zoom.rb
@@ -4,5 +4,5 @@
 Deface::Override.new(:virtual_path => "spree/products/_thumbnails",
                       :name => 'replace-thumbnail-link-with-joom-link',
                       :replace => "code[erb-loud]:contains('link_to')") do
-                        "<div id='product_info_thumbnails'><a href=\"<%= @product.images.first.attachment.url(:large) %>\" rel=\"{gallery: 'product-gallery', smallimage: '<%= i.attachment.url(:large) %>', largeimage: '<%= i.attachment.url(:large) %>'}\" class=\"<%= (i.id == @product.images.first.id) ? 'zoomThumbActive' : '' %> \" > <%= image_tag(i.attachment.url(:small)) %></a></div>"
+                        "<div id='product_info_thumbnails'><a href=\"<%= @product.images.first.attachment.url(:large) %>\" rel=\"{gallery: 'product-gallery', smallimage: '<%= i.attachment.url(:large) %>', largeimage: '<%= i.attachment.url(:large) %>'}\" class=\"<%= (i.id == @product.images.first.id) ? 'zoomThumbActive' : '' %> \" > <%= image_tag(i.attachment.url(:small), :class => 'product_info_thumbnail') %></a></div>"
                       end
