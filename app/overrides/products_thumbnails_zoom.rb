9eb5467de1c615d8a70453e93ebe38d10fcc9459
damageless <damageless@gmail.com>
2012-12-07 10:02:28 -0700

    Updating to have custom div inside of thumbnail view

--- app/overrides/products_thumbnails_zoom.rb
+++ app/overrides/products_thumbnails_zoom.rb
@@ -4,5 +4,5 @@
 Deface::Override.new(:virtual_path => "spree/products/_thumbnails",
                       :name => 'replace-thumbnail-link-with-joom-link',
                       :replace => "code[erb-loud]:contains('link_to')") do
-                        "<a href=\"<%= @product.images.first.attachment.url(:large) %>\" rel=\"{gallery: 'product-gallery', smallimage: '<%= i.attachment.url(:large) %>', largeimage: '<%= i.attachment.url(:large) %>'}\" class=\"<%= (i.id == @product.images.first.id) ? 'zoomThumbActive' : '' %> \" > <%= image_tag(i.attachment.url(:small)) %></a>"
+                        "<div id='product_info_thumbnails'><a href=\"<%= @product.images.first.attachment.url(:large) %>\" rel=\"{gallery: 'product-gallery', smallimage: '<%= i.attachment.url(:large) %>', largeimage: '<%= i.attachment.url(:large) %>'}\" class=\"<%= (i.id == @product.images.first.id) ? 'zoomThumbActive' : '' %> \" > <%= image_tag(i.attachment.url(:small)) %></a></div>"
                       end
